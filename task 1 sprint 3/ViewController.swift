98aa59af4da2242fca053aaf552cac39b56ed788
AryunaSan <tudupovaa@gmail.com>
2024-02-28 18:12:50 +0300

    выполнена базовая задача

--- task 1 sprint 3/ViewController.swift
+++ task 1 sprint 3/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBAction func pressedButton(_ sender: Any) {buttonPresses += 1
+        label.text = "Значение счетчика:  \(buttonPresses)"
+    }
+    var buttonPresses = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
